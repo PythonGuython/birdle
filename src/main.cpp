2a799ccc42bb09fe68b47571871bb34d36170954
Hunter Halloran <hdhdog@live.com>
2022-02-19 04:13:02 -0500

    ir pins

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
 #include <MFRC522.h>
 #include <SPI.h>
+#include <IRremote.hpp>
+
+IRsend **irPins = new IRsend*[5];
+
+// Store words in PROGMEM
+const char PROGMEM words[][6] = {"abcde", "ghijk", "mnopq", "rstuv"};
+
 
 void setup() {
   // put your setup code here, to run once:
+    Serial.begin(9600);
+    // Set irPins to act on pins 1 to 5
+    for (int i = 0; i < 5; i++) {
+        irPins[i] = new IRsend(i + 1);
+    } 
 }
 
 void loop() {
